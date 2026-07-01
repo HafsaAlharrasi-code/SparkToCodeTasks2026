cbb0fecd4b915957288e44ee333a5036cabd8f2d
hafsa <hafsaalharrasi9@gmail.com>
2026-07-01 19:09:46 +0400

    problem1 implementation

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+        /*
         //1-personal info card
+        string userName = "Hafsa";
+        int age = 21;
+        double gpa = 3.11;
+        bool student = true;
+        Console.WriteLine("Name: " + userName + " ,Age: "+ age +" ,GPA: "+gpa + " ,Student: "+student);
+        //////////////////////////////////////
+        */
+        
         
     }
 }
