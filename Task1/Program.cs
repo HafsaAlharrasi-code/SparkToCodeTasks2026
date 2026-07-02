9368d68d7ea1cdeadabb79537d72e79f6ab84b1b
hafsa <hafsaalharrasi9@gmail.com>
2026-07-02 16:23:44 +0400

    Sum of Numbers 1 to N Implementation

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        /*
         //1- Countdown Timer
         Console.WriteLine("Enter a starting number:");
         int startNumber = int.Parse(Console.ReadLine());
@@ -13,5 +13,16 @@ class Program
             Console.WriteLine(i);
         }
         Console.WriteLine("Liftoff");
+        */
+        
+        //2-Sum of Numbers 1 to N
+        Console.WriteLine("Enter a positive whole number N: ");
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine("The sum is: "+sum);
     }
 }
