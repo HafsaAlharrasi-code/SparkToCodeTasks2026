57a4650c67fc77f9c94142a34a432d4f0f37ac44
hafsa <hafsaalharrasi9@gmail.com>
2026-07-02 16:18:51 +0400

    Countdown Timer Implementation

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,6 +5,13 @@ class Program
     static void Main(string[] args)
     {
         
-        Console.WriteLine("Hello World!");
+        //1- Countdown Timer
+        Console.WriteLine("Enter a starting number:");
+        int startNumber = int.Parse(Console.ReadLine());
+        for (int i = startNumber; i >=1 ; i--)
+        {
+            Console.WriteLine(i);
         }
+        Console.WriteLine("Liftoff");
     }
+}
