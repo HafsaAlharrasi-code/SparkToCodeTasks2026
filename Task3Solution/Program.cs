ca2e6ac00b8a453e06daf6794796cbb055c898a8
hafsa <hafsaalharrasi9@gmail.com>
2026-07-05 22:51:44 +0400

    (Absolute Difference) Implementation

--- Task3Solution/Program.cs
+++ Task3Solution/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        //1- Absolute Difference
+        
+        Console.WriteLine("Enter a number: ");
+        double number1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter another number: ");
+        double number2 = double.Parse(Console.ReadLine());
+        
+        double result = number1 - number2;
+        double absResult = Math.Abs(result);
+        Console.WriteLine("The subtraction result is: "+absResult);
+        
     }
 }
