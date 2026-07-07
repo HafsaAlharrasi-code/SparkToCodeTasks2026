9e4ef53e658eac59606c26604ad4dfef96a850b4
hafsa <hafsaalharrasi9@gmail.com>
2026-07-07 21:00:31 +0400

    (Fixed Grades Array) Implementation

--- Task5Solution/Program.cs
+++ Task5Solution/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //1-Fixed Grades Array
+        int[] grades = new int[5];
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.WriteLine("Enter grade: ");
+            grades[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Grades: ");
+        foreach (int grade in grades)
+        {
+            Console.WriteLine(grade);
+        }
     }
 }
