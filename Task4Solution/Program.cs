d2dad428cad9583b22db2fdb35bb29d058c6b93b
hafsa <hafsaalharrasi9@gmail.com>
2026-07-07 18:26:08 +0400

    (Celsius to Fahrenheit Function) Implementation

--- Task4Solution/Program.cs
+++ Task4Solution/Program.cs
@@ -12,11 +12,20 @@ class Program
         Console.WriteLine();
         */
         
+        /*
         //2-Square Number Function
         Console.WriteLine("Enter your a number:");
         int userNumber = int.Parse(Console.ReadLine());
         int squaredNum = Square(userNumber);
         Console.WriteLine("The squared number is: " + squaredNum);
+        Console.WriteLine();
+        */
+        
+        //3-Celsius to Fahrenheit Function
+        Console.Write("Enter temperature in Celsius: ");
+        double celsius = double.Parse(Console.ReadLine());
+        double fahrenheit = CelsiusToFahrenheit(celsius);
+        Console.WriteLine("Fahrenheit temperature is: " + fahrenheit);
         Console.WriteLine();
     }
     
@@ -28,10 +37,18 @@ class Program
     }
     */
     
+    /*
     //2-Square Number Function
     static int Square(int number)
     {
         return (number * number);
+    }
+    */
+
+    static double CelsiusToFahrenheit(double celsius)
+    {
+        double fahrenheit = (celsius * 9) / 5 + 32; 
+        return fahrenheit;
     }
     
     
