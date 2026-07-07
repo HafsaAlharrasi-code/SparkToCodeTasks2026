5e41304c247f37cd4f297a3687c7a09533390ed8
hafsa <hafsaalharrasi9@gmail.com>
2026-07-07 18:19:48 +0400

    (Square Number Function) Implementation

--- Task4Solution/Program.cs
+++ Task4Solution/Program.cs
@@ -4,18 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-       
+        /*
         //1-Personalized Welcome Function
         Console.WriteLine("Enter your name:");
         string name = Console.ReadLine();
         PrintWelcome(name);
         Console.WriteLine();
+        */
         
+        //2-Square Number Function
+        Console.WriteLine("Enter your a number:");
+        int userNumber = int.Parse(Console.ReadLine());
+        int squaredNum = Square(userNumber);
+        Console.WriteLine("The squared number is: " + squaredNum);
+        Console.WriteLine();
     }
+    
+    /*
     //1-Personalized Welcome Function
     static void PrintWelcome(string userName)
     {
         Console.WriteLine("Welcome, " + userName);
     }
+    */
+    
+    //2-Square Number Function
+    static int Square(int number)
+    {
+        return (number * number);
+    }
+    
     
 }
