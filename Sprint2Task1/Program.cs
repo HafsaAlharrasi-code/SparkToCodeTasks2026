0adb61bfb665629d3852c5862417ef2b5f6abb9d
hafsa <hafsaalharrasi9@gmail.com>
2026-07-13 20:31:38 +0400

    BankAccount class implementation

---
+++ Sprint2Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Sprint2Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    
+}
+
+public class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public string HolderName { get; set; }
+    public double Balance { get; set; }
+    
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+        SendEmail();
+    }
+    public void Withdraw(double amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+        }
+        SendEmail();
+    }
+    public double CheckBalance()
+    {
+        PrintInformation();
+        return Balance;
+    }
+    private void PrintInformation()
+    {
+        Console.WriteLine("Name : " + HolderName);
+        Console.WriteLine("Balance: " + Balance);
+    }
+    private void SendEmail(){}
+}
