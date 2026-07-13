0d36317b7e5177e75e3257a4d8d7460ae1c37528
hafsa <hafsaalharrasi9@gmail.com>
2026-07-13 20:47:30 +0400

    Student class implementation

--- Sprint2Task1/Program.cs
+++ Sprint2Task1/Program.cs
@@ -39,3 +39,19 @@ public class BankAccount
     }
     private void SendEmail(){}
 }
+
+public class Student
+{
+    public int Grade { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    private string email { get; set; }
+    int age { get; set; }
+    
+    public void Register(string Email)
+    {
+        email = Email;
+        SendEmail();
+    }
+    private void SendEmail(){}
+}
