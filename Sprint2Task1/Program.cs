cc66807b13705a883a5b0f82a786a62c7edfeea3
hafsa <hafsaalharrasi9@gmail.com>
2026-07-13 21:13:20 +0400

    print menu

--- Sprint2Task1/Program.cs
+++ Sprint2Task1/Program.cs
@@ -33,6 +33,43 @@ class Program
         product2.ProductName = "Mechanical Keyboard";
         product2.Price = 15.750;
         product2.StockQuantity = 20;
+        
+        bool exitApp = false;
+        while (exitApp == false)
+        {
+            Console.WriteLine(" 1. View Account Details");
+            Console.WriteLine(" 2. Update Student Address");
+            Console.WriteLine(" 3. Make a Deposit");
+            Console.WriteLine(" 4. Make a Withdrawal");
+            Console.WriteLine(" 5. View Product Details");
+            Console.WriteLine(" 6. Register a Student");
+            Console.WriteLine(" 7. Compare Two Account Balances");
+            Console.WriteLine(" 8. Restock Product & Stock Level Check");
+            Console.WriteLine(" 9. Transfer Between Accounts");
+            Console.WriteLine("10. Update Student Grade (Validated)");
+            Console.WriteLine("11. Student Report Card");
+            Console.WriteLine("12. Account Health Status");
+            Console.WriteLine("13. Bulk Sale With Revenue Calculation");
+            Console.WriteLine("14. Scholarship Eligibility Check");
+            Console.WriteLine("15. Full Balance Top-Up Flow");
+            Console.WriteLine("16. Quick Account Opening ");
+            Console.WriteLine("17. Total Students Counter ");
+            Console.WriteLine("18. Overdrawn Account Check ");
+            Console.WriteLine("19. Set Student Security PIN )");
+            Console.WriteLine("20. Exit");
+            Console.Write("Choose an option: ");
+            
+            int choice;
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input. Enter a number from 1 to 20.");
+                continue;
+            }
+        }
 
     }
     
